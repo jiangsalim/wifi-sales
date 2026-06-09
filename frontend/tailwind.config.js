ab2f54ad83ce0ca24e7f8259c1ace66ca1844ac2
jiangsalim <jaingsalim@gmail.com>
2026-06-09 22:57:40 +0300

    Add responsive container-site to all pages, Tailwind config, update Navbar and Footer

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      maxWidth: {
+        'site': '1200px',
+      },
+    },
+  },
+  plugins: [],
+}
